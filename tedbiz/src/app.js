96fef4154f43d5e79d4fb64e1caba6d199cc833a
Leland Clemmons <leland.clemmons@gmail.com>
2022-03-27 17:32:45 -0400

    progress

---
+++ tedbiz/src/app.js
@@ -0,0 +1,78 @@
+const { algoliasearch, instantsearch } = window;
+
+const searchClient = algoliasearch(
+  'G536CNR030',
+  '6172dacfd1fed56ea6d64f0e63556a63'
+);
+
+const search = instantsearch({
+  indexName: 'TEDbiz',
+  searchClient,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: `
+<article class="ais-Hits-item__content">
+  <h1 class="ais-Hits-item__header">{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</h1>
+  <p class="ais-Hits-item__description">{{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}</p>
+  <p class="ais-Hits-item__speakers">{{#helpers.highlight}}{ "attribute": "speakers" }{{/helpers.highlight}}</p>
+  <p class="ais-Hits-item__event">{{#helpers.highlight}}{ "attribute": "event_name" }{{/helpers.highlight}}</p>
+</article>
+`,
+    },
+  }),
+  instantsearch.widgets.configure({
+    facets: ['*'],
+    maxValuesPerFacet: 20,
+  }),
+  instantsearch.widgets.dynamicWidgets({
+    container: '#dynamic-widgets',
+    fallbackWidget({ container, attribute }) {
+      return instantsearch.widgets.refinementList({
+        container,
+        attribute,
+      });
+    },
+    widgets: [
+      container =>
+        instantsearch.widgets.refinementList({
+          container,
+          attribute: 'tags',
+        }),
+      container =>
+        instantsearch.widgets.refinementList({
+          container,
+          attribute: 'speakers',
+        }),
+      container =>
+        instantsearch.widgets.refinementList({
+          container,
+          attribute: 'salePrice',
+        }),
+      container =>
+        instantsearch.widgets.refinementList({
+          container,
+          attribute: 'bestSellingRank',
+        }),
+    ],
+  }),
+  instantsearch.widgets.menu({
+    container: '#menu',
+    attribute: 'tags',
+  }),
+  instantsearch.widgets.rangeSlider({
+    container: '#range-slider',
+    attribute: 'salePrice',
+    min: 40,
+    max: 200,
+    pips: false,
+  }),
+]);
+
+search.start();
